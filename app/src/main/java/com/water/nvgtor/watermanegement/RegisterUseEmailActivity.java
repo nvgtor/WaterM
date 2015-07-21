cd794376c866e449c3fa13028871c1b1b4a080a3
nvgtor <nvgtor.ze@gmail.com>
2015-07-21 10:16:27 +0800

    ui
    forgetPassword ui

---
+++ app/src/main/java/com/water/nvgtor/watermanegement/RegisterUseEmailActivity.java
@@ -0,0 +1,7 @@
+package com.water.nvgtor.watermanegement;
+
+/**
+ * Created by dell on 2015/7/20.
+ */
+public class RegisterUseEmailActivity {
+}
---
+++ app/src/main/java/com/water/nvgtor/watermanegement/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.water.nvgtor.watermanegement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by dell on 2015/7/20.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.welcome);
+    }
+}
