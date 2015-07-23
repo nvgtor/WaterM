9df28471c07520d4b0620d325a7d70cf1e61fcd4
nvgtor <nvgtor.ze@gmail.com>
2015-07-23 15:14:49 +0800

    GridView

--- app/src/main/java/com/water/nvgtor/watermanegement/fragment/FunFragment.java
+++ app/src/main/java/com/water/nvgtor/watermanegement/fragment/FunFragment.java
@@ -1,32 +1,35 @@
 package com.water.nvgtor.watermanegement.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.GridView;
 
 import com.water.nvgtor.watermanegement.R;
+import com.water.nvgtor.watermanegement.adapter.MyGridAdapter;
 
 /**
  * Created by dell on 2015/7/22.
  */
 public class FunFragment extends Fragment {
-    String text;
+    //String text;
+    private GridView gridView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Bundle args = getArguments();
-        text = args != null ? args.getString("text") : "";
+        /*Bundle args = getArguments();
+        text = args != null ? args.getString("text") : "";*/
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.function_fragment, null);
-        TextView item_textView = (TextView)view.findViewById(R.id.item_textview);
-        item_textView.setText(text);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_function, null);
+        gridView = (GridView)view.findViewById(R.id.gridview);
+        MyGridAdapter adapter = new MyGridAdapter(getActivity().getApplicationContext());
+        gridView.setAdapter(adapter);
         return view;
     }
 }
